4bb995b936061677c5861f7f64c1b36c790b7737
Tomascabfer4 <93979753+Tomascabfer4@users.noreply.github.com>
2022-03-10 09:25:14 +0100

    Update agenda_borrar_tomas.py

--- agenda_borrar_tomas.py
+++ agenda_borrar_tomas.py
@@ -4,7 +4,6 @@
 ###te responde pidiendote el pulsado de la tecla s para confirmar el borrado###
 ###Si no existe el contacto simplemente el programa responde diciendo que no existe.###  
 def borrar_Tcabello(nombre,agenda):
-    nombre = input("Nombre del contacto a borrar: ")
     if nombre in agenda:
         opcion = input("Pulsa 's' si quieres borrarlo. Otra tecla para continuar.")
         if opcion == "s":
